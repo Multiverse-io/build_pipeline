273caa817867e1d1ddf28ec5b76ff16ef4257117
Matthew Berners-Lee <matthewbernerslee@gmail.com>
2023-02-10 14:58:32 +0000

    bumping package version

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule BuildPipeline.MixProject do
       app: :build_pipeline,
       description: description(),
       package: package(),
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
